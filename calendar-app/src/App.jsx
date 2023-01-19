bbf5cd1a8156f28f0e3d7aac5999dd74fcdad7ec
Seann85 <koyun.gok2@gmail.com>
2023-01-19 12:20:43 +0000

    add features

--- calendar-app/src/App.jsx
+++ calendar-app/src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React from "react";
+import { generateDate } from "./util/calendar";
 
 const App = () => {
+  console.log(generateDate());
+
+  const days = ["M","T","W","T","F","S","S"];
+  
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-500 text-center">Hello world!</h1> 
+  
+  <div className="w-96 h-96 ">
 
+    <div className="w-full grid grid-cols-7"> 
+      {days.map((day,index)=>{
+      return <h1 key={index}>{day}</h1>})}
     </div>
-  )
-}
+  <div className="w-full grid grid-cols-7">
 
-export default App
+
+{generateDate().map(({date, currentMonth, today},index)=>{
+
+  return <div key={index}>
+    <h1>{date.date()}</h1>
+  </div>
+})}
+
+</div>
+  </div>
+)};
+
+export default App;
---
+++ calendar-app/src/util/calendar.js
@@ -0,0 +1,37 @@
+import dayjs from "dayjs";
+
+export const generateDate = (
+  month = dayjs().month(),
+  year = dayjs().year()
+) => {
+  const firstDateOfMonth = dayjs().year(year).month(month).startOf("month");
+  const lastDateOfMonth = dayjs().year(year).month(month).endOf("month");
+
+  const arrayOfDate = [];
+
+  for (let i = 0; i < firstDateOfMonth.day(); i++) {
+    arrayOfDate.push({ currentMonth: false, date: firstDateOfMonth.day(i) });
+  }
+
+  for (let i = firstDateOfMonth.date(); i <= lastDateOfMonth.date(); i++) {
+    arrayOfDate.push({
+      date: firstDateOfMonth.date(i),
+      currentMonth: true,
+      today:
+        firstDateOfMonth.date(i).toDate().toDateString() ===
+        dayjs().toDate().toDateString(),
+    });
+  }
+
+  const remaining = 42 - arrayOfDate.length;
+
+  for (
+    let i = lastDateOfMonth.date() + 1;
+    i <= lastDateOfMonth.date() + remaining;
+    i++
+  ) {
+    arrayOfDate.push({ date: lastDateOfMonth.day(i), currentMonth: false });
+  }
+
+  return arrayOfDate;
+};
